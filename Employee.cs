c0a6f812f8565e41029607d4fd3162ac3c440b91
aslan0213 <aslanmamedov0213@gmail.com>
2025-03-14 00:24:52 +0400

    Simple tasks compleated

---
+++ Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Employee
+    {
+      
+		private string Name;
+
+		public string name
+		{
+			get { return Name; }
+			set { Name = (!string.IsNullOrEmpty(value))?value:throw new ArgumentException("Name cannot be empty."); }
+		}
+
+		private double HourlyRate;
+
+		public double hourlyRate
+		{
+			get { return HourlyRate; }
+			set { HourlyRate = (value>0)?value:throw new ArgumentException("Rate can not be zero o negative."); }
+		}
+		private double HoursWorked;
+
+		public double hoursWorked
+		{
+			get { return HoursWorked; }
+			set { HoursWorked = (value>=0)?value:throw new ArgumentException("Number of hours worled can not be negative."); }
+		}
+        public Employee(string name, double hourlyRate,double hoursWorked)
+        {
+			this.name = name;
+			this.hourlyRate = hourlyRate;
+			this.hoursWorked = hoursWorked;
+        }
+		public decimal CalculateSalary()
+		{
+			return (decimal)(HourlyRate * HoursWorked);
+		} 
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,10 +8,12 @@
             ////Task1
             //TemperatureConverter tc = new TemperatureConverter(100);
             //Console.WriteLine(tc.Fahrenheit);
-            //Task2
+            ////Task2
             //Product p = new Product("Pen", 20, -1000);
             //Console.WriteLine($"Product name: {p.Name}, price: {p.Price}, stock: {p.Stock}");
-            //Task3 
+            ////Task3 
+            //Employee emp1 = new Employee("Aslan", 2.5, 2000);
+            //Console.WriteLine($"Name: {emp1.name}; salary: {emp1.CalculateSalary()}");
 
             #endregion
 
